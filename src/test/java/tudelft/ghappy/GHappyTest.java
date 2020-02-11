8e5fac33174332b48b88435b5b8b1b49ac492c3d
djweigand@hpnotebook <djweigand-1@tudelft.nl>
2020-02-11 14:32:54 +0100

    2-1 GHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,35 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "{0}: ({1}, {2}, {3}) = {4}")
+    @CsvSource({
+            // Invalid input
+            "'invalid','ashuj',true", "'invalid','0',true", "'invalid','',true"
+    })
+    public void invalid(String partition, String str, boolean expectedResult) {
+        boolean result = new GHappy().gHappy(str);
+        Assertions.assertEquals(expectedResult, result);
+    }
+    @ParameterizedTest(name = "{0}: ({1}, {2}, {3}) = {4}")
+    @CsvSource({
+            // No need for small bars.
+            "'single','g',true", "'single','gag',true", "'single','gaga',true"
+    })
+    public void single(String partition, String str, boolean expectedResult) {
+        boolean result = new GHappy().gHappy(str);
+        Assertions.assertEquals(expectedResult, result);
+    }
+    @ParameterizedTest(name = "{0}: ({1}, {2}, {3}) = {4}")
+    @CsvSource({
+            // multiple
+            "'multiple','gg',false", "'multiple','gaggg',false"
+    })
+    public void multiple(String partition, String str, boolean expectedResult) {
+        boolean result = new GHappy().gHappy(str);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
